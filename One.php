d4ae685c049ec493910d27b8294cb93b8eaeaadd
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2021-06-10 01:00:29 +0200

    Create One.php

---
+++ One.php
@@ -0,0 +1,37 @@
+
+
+<?php 
+
+function even_odd($number) {
+     $sum = 0;
+
+//Checking if the number is even 
+if ($number % 2 == 0){
+     echo "It is a even number ";
+
+     // printing the sum that is 0+1+2+3....(number entered)
+
+for ($i = 0; $i <= $number; $i ++ ){
+$sum = $sum + $i;
+
+
+}
+echo " <br> The sum of number is =$sum";
+
+}
+
+else {
+     echo "It is odd number ";
+}
+
+
+}
+
+// Enter number to search 
+$number = 110;
+
+even_odd($number);
+
+
+
+ ?>
